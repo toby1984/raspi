a3f98b1bf5107d9d2ad7a505da4ef727e81cb5f6
Tobias Gierke <tobias.gierke@code-sourcery.de>
2018-04-29 12:40:07 +0200

    initial commit

---
+++ library/render/src/global.h
@@ -0,0 +1,14 @@
+
+#ifndef GLOBAL_H
+#define GLOBAL_H
+
+#include "SDL/SDL.h"
+// #include "SDL/SDL_ttf.h"
+// #include "SDL/SDL_getenv.h"
+
+// #define USE_FB
+
+// Define the primary surface for display
+SDL_Surface* scrMain = NULL;
+#endif
+
